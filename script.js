8d141c68a9527d7967bad97d9d56f5d41ce73f5f
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-11 14:32:17 +0100

    for e if dei numeri divisibili 3, 5, 3 e 5

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //stampa numeri 1-100, *3(fizz), *5(buzz), *3*5(fizzbuzz)
 //stampa numeri 1-100
-//if i%3==0 fizz, if else i%5==0 buzz, if else 1&&2 fizzbuzz, else
+//if i%3==0 fizz, else if i%5==0 buzz, else if 1&&2 fizzbuzz, else
 for(let i = 1; i <= 100; i++){
-    console.log(i);
+    if(i %3 == 0 && i %5 == 0){
+        console.log("FizzBuzz");
+    } else if(i %3 == 0){
+        console.log("Fizz");
+    } else if(i %5 == 0){
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
 }
