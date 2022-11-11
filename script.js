7ba4ccc6d9ed81f6b50d26b6b2f662f6d024287f
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-11 14:26:20 +0100

    stampa numeri da 1 a 100

---
+++ script.js
@@ -0,0 +1,6 @@
+//stampa numeri 1-100, *3(fizz), *5(buzz), *3*5(fizzbuzz)
+//stampa numeri 1-100
+//if i%3==0 fizz, if else i%5==0 buzz, if else 1&&2 fizzbuzz, else
+for(let i = 1; i <= 100; i++){
+    console.log(i);
+}
